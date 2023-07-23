19c63c67110808e64a91eb1e4d3ec1d8176ef3a1
camperfail86 <mega.kochkin@inbox.ru>
2023-07-23 17:37:19 +0500

    Добавляет swiper

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,1 +1,15 @@
-console.log(1);
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  effect: 'slider',
+  spaceBetween: 72,
+  slidesPerView: 2,
+  centeredSlidesBounds: true,
+  initialSlide: 1, 
+  loop: true,
+  speed: 650,
+
+  navigation: {
+    nextEl: '.swiper-btn-next',
+    prevEl: '.swiper-btn-prev',
+  },
+});
